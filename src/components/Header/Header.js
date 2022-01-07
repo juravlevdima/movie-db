2c28dfb53d8f46b4da5c6bb3a145909ffd958e63
Juravlev Dmitrii <juravlevdima96@gmail.com>
2022-01-07 12:22:56 +0600

    hide main button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
         <div className="d-flex justify-content-between align-items-center">
           <Link to="/"><img className="logo" src={logo} alt="TMDB"/></Link>
           <nav>
-            <NavLink to="/" className="btn btn-success me-2">Главная</NavLink>
+            {/*<NavLink to="/" className="btn btn-success me-2">Главная</NavLink>*/}
             <NavLink to="/films?page=1" className="btn btn-success me-2">Фильмы</NavLink>
             <NavLink to="/serials?page=1" className="btn btn-success">Сериалы</NavLink>
           </nav>
