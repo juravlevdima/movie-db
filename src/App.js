20297ee14a9c9394d22880d1f48d78b76d2ab5b6
Juravlev Dmitrii <juravlevdima96@gmail.com>
2022-01-10 21:10:42 +0600

    add lang change fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Routes, Route, Navigate} from 'react-router-dom'
 import Main from "./pages/Main/Main";
 import AllFilms from "./pages/AllFilms/AllFilms";
